e93a7f93d4007a1618f73c5778f67d861c2aefa9
Franklin Alvarado <franklinalvarado0921@gmail.com>
2023-01-24 10:23:44 -0500

    refactor app

--- projects/02-tic-tac-toe/src/App.jsx
+++ projects/02-tic-tac-toe/src/App.jsx
@@ -1,44 +1,13 @@
 import { useState } from 'react'
+import { Square } from './components/Square'
+import { TURNS, WINNER_COMBOS } from './constants'
+import confetti from 'canvas-confetti'
 import './App.css'
-
-const TURNS = {
-  X: 'X',
-  O: 'O'
-}
-
-const Square = ({ children, isSelected, updateBoard, index }) => {
-  const className = `square ${isSelected ? 'is-selected' : ''}`
-
-  const handleClick = () => {
-    updateBoard(index)
-  }
-
-  return (
-    <div onClick={handleClick} className={className}>
-      {children}
-    </div>
-  )
-}
-
-const WINNER_COMBOS = [
-  // verticales
-  [0, 1, 2],
-  [3, 4, 5],
-  [6, 7, 8],
-  // horizzontales
-  [0, 3, 6],
-  [1, 4, 7],
-  [2, 5, 8],
-  // diagonales
-  [0, 4, 8],
-  [6, 4, 2]
-]
 
 function App () {
   const [board, setBoard] = useState(Array(9).fill(null))
   const [turn, setTurn] = useState(TURNS.X)
-  // null no hay ganador, false empate.
-  const [winner, setWinner] = useState(null)
+  const [winner, setWinner] = useState(null) // null no hay ganador, false empate.
 
   const checkWinner = (boardToCheck) => {
     for (const combo of WINNER_COMBOS) {
@@ -54,6 +23,11 @@ function App () {
     }
     // si no hay ganador
     return null
+  }
+
+  const checkEndGame = (newBoard) => {
+    // Hay empate si no hay espacios null en el tablero
+    return newBoard.every((square) => square !== null)
   }
 
   const updateBoard = (index) => {
@@ -73,13 +47,22 @@ function App () {
     const newWinner = checkWinner(newBoard)
     if (newWinner) {
       setWinner(newWinner)
-      window.alert(`Winner is ${newWinner}`)
+      confetti()
+    } else if (checkEndGame(newBoard)) {
+      return setWinner(false) // empate
     }
+  }
+
+  const resetGame = () => {
+    setBoard(Array(9).fill(null))
+    setTurn(TURNS.X)
+    setWinner(null)
   }
 
   return (
     <main className='board'>
       <h1>Tic Tac Toe</h1>
+      <button onClick={resetGame}>reset juego</button>
       <section className='game'>
         {
           board.map((cell, index) => (
@@ -98,6 +81,30 @@ function App () {
         <Square isSelected={turn === TURNS.X}>{TURNS.X}</Square>
         <Square isSelected={turn === TURNS.O}>{TURNS.O}</Square>
       </section>
+
+      {
+        winner !== null && (
+          <section className='winner'>
+            <div className='text'>
+              <h2>
+                {
+                  winner === false
+                    ? 'empate'
+                    : 'Ganó'
+                }
+              </h2>
+
+              <header className='win'>
+                {winner && <Square>{winner}</Square>}
+              </header>
+
+              <footer>
+                <button onClick={resetGame}>empezar denuevo</button>
+              </footer>
+            </div>
+          </section>
+        )
+      }
     </main>
   )
 }
