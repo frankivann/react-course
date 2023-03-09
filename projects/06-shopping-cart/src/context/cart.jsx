631b0f7d8d643032d504457c693c04943dc8c3f8
Franklin Alvarado <franklinalvarado0921@gmail.com>
2023-03-09 11:36:58 -0500

    add useCartReducer

--- projects/06-shopping-cart/src/context/cart.jsx
+++ projects/06-shopping-cart/src/context/cart.jsx
@@ -1,62 +1,9 @@
-import { createContext, useReducer } from 'react'
-
+import { createContext } from 'react'
+import { useCartReducer } from '../hooks/useCartReducer'
 export const CartContext = createContext()
 
-const initialState = []
-const reducer = (state, action) => {
-  console.log(state)
-  console.log(action)
-
-  const { type, payload } = action
-
-  switch (type) {
-    case 'ADD_TO_CART': {
-      const { id } = payload
-      const productInCartIndex = state.findIndex(item => item.id === id)
-
-      if (productInCartIndex >= 0) {
-        const newState = structuredClone(state)
-        newState[productInCartIndex].quantity += 1
-        return newState
-      }
-
-      return [
-        ...state,
-        {
-          ...payload, // product
-          quantity: 1
-        }
-      ]
-    }
-
-    case 'REMOVE_FROM_CART': {
-      const { id } = payload
-      return state.filter(item => item.id !== id)
-    }
-
-    case 'CLEAR_CART': {
-      return initialState
-    }
-
-    default:
-      break
-  }
-}
-
 export function CartProvider ({ children }) {
-  const [state, dispatch] = useReducer(reducer, initialState)
-
-  const addToCart = product => dispatch({
-    type: 'ADD_TO_CART',
-    payload: product
-  })
-
-  const removeFromCart = product => dispatch({
-    type: 'REMOVE_FROM_CART',
-    payload: product
-  })
-
-  const clearCart = () => dispatch({ type: 'CLEAR_CART' })
+  const { addToCart, clearCart, removeFromCart, state } = useCartReducer()
 
   const value = {
     cart: state,
@@ -65,5 +12,11 @@ export function CartProvider ({ children }) {
     clearCart
   }
 
-  return <CartContext.Provider value={value}>{children}</CartContext.Provider>
+  return (
+    <CartContext.Provider
+      value={value}
+    >
+      {children}
+    </CartContext.Provider>
+  )
 }
